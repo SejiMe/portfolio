b37f3d33938cc6dda9aa66e1fd2d1dc177041169
SejiMe <atienza.seji@gmail.com>
2023-02-17 07:10:46 +0800

    chore: add loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,41 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import Router from "next/router";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import { useState } from "react";
+import Loader from "../components/Loader";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+    Component,
+    pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+    const [loading, setLoading] = useState(false);
+    Router.events.on("routeChangeStart", () => {
+        setLoading(true);
+    });
+
+    Router.events.on("routeChangeComplete", () => {
+        setLoading(false);
+    });
+    return (
+        <SessionProvider session={session}>
+            {loading ? (
+                <div className='flex flex-col h-screen w-screen items-center justify-center bg-blue-500'>
+                    <div>
+                        <h1 className="animate-pulse text-beige-500">
+                            Relax, You are about to enter the Portal
+                        </h1>
+                    </div>
+                    <Loader />
+                </div>
+            ) : (
+                <Component {...pageProps} />
+            )}
+        </SessionProvider>
+    );
 };
 
 export default api.withTRPC(MyApp);
