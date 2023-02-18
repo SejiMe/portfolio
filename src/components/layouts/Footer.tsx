0124182a038b84e593ad9c529e24912a72bf72a1
SejiMe <atienza.seji@gmail.com>
2023-02-18 11:09:41 +0800

    chore: Footer Dividier

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,11 +1,43 @@
+import clsx from "clsx";
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div></div>
+        <footer className='bg-beige-400 mt-10'>
+            <h1>This is Footer</h1>
+            <div className='grid grid-flow-col gap-4'>
+                <Divider>
+                    <h2>1</h2>
+                </Divider>
+                <Divider>
+                    <h2>2</h2>
+                </Divider>
+                <Divider>
+                    <h2>3</h2>
+                </Divider>
+            </div>
     </footer>
   );
 };
 
 export default Footer;
+
+// This is a div container
+const Divider = ({
+    children,
+    classname,
+}: {
+    children?: React.ReactNode;
+    classname?: string;
+}) => {
+    return (
+        <div
+            className={clsx(
+                "flex justify-center border-2 border-teal-400 p-4",
+                classname
+            )}
+        >
+            {children}
+        </div>
+    );
+};
