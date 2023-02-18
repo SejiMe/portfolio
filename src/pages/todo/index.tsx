463f7fe2c3e93b68d4bbb2d4f2a2522a210572b4
SejiMe <atienza.seji@gmail.com>
2023-02-18 11:11:17 +0800

    chore: sections and classes

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,13 +1,30 @@
 import { NextPage } from "next";
 import React from "react";
 import MainApp from "../../components/layouts/MainApp";
+import Button from "../../components/customs/buttons/Button";
 
 const Todo: NextPage = () => {
+    console.log('object');
     return (
-        <MainApp>
-            <section>
+        <MainApp classname=''>
+            <section className='h-screen'>
                 <h1>Hi! What&apos;s up?</h1>
+                <div className='grid grid-cols-4 grid-rows-4 p-2 gap-2 border-2 border-white'>
+                    <div className='col-start-4 bg-blue-100 content-center align-middle items-center'>
+                        <Button
+                            type='button'
+                            classname='p-2'
+                        >
+                            Add Task
+                        </Button>
+                    </div>
+                    <div className='rounded-md bg-slate-200 col-start-1 col-end-3 row-span-2'></div>
+                    <div className='rounded-md bg-orange-200 row-span-3 col-start-3 col-end-6'></div>
+                    <div className='rounded-md bg-lime-200'></div>
+                    <div className='rounded-md bg-green-200'></div>
+                </div>
             </section>
+            <section></section>
         </MainApp>
     );
 };
