ee4a62cdde5ecef095786d54e0210e031bb1fa05
SejiMe <atienza.seji@gmail.com>
2024-12-01 10:19:53 +0800

    Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import robotsTxt from "astro-robots-txt";
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 import sitemap from "@astrojs/sitemap";
-
+const LIVE_URL = "https://sejime.github.io/portfolio"
+const LOCAL_URL = "https://localhost:4000"
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     rehypePlugins: [rehypeAccessibleEmojis],
   },
-  site: 'https://astro-portfolio-template.pages.dev',
+  site: LIVE_URL,
+  base: "/portfolio",
   integrations: [tailwind(), react(), robotsTxt(), sitemap()]
 });
--- src/data/info.ts
+++ src/data/info.ts
@@ -9,26 +9,33 @@ export const info = {
   cv: "/resume.pdf",
 
   about: {
-    description: `I'm a full stack developer with a passion for causing a little chaos in web and backend development.
-    I have a solid foundation in Information Systems, although I prefer playing drums over studying.
-    I excel in my applications using the technologies .NET, Node.JS, SQL Server, TypeScript
-    I'm known for my sharp wit and quick thinking, traits that serve me well in coding and pranks alike.
+    description: `I'm a full stack developer with a passion for causing a little chaos in desktop, web and backend development.
+    I have a solid foundation in Information Systems, and using Vertical.
+    I excel in building my applications using the technologies C#, .NET, Node.JS, SQL Server, NextJS, TypeScript
     Looking for a challenge? Let's create something memorable together.`,
     education: [
+      {
+        title: "Bachelors of Science in Information System",
+        date: "2019 - 2023",
+        location: "Philippines Womens' University, Tarlac City, Philippines",
+        gpa: "",
+        thesis: "Barangay Web Application Information and Document Management System",
+      },
+      {
+        title: "Diploma in Information System Technology",
+        date: "2019 - 2022",
+        location: "Philippines Womens' University, Tarlac City, Philippines",
+        gpa: "",
+        thesis: "",
+      },
       {
         title: "Senior High School Academic Track on Accountancy & Business Management",
         date: "2017-2019",
         location: "St. Rose Catholic School, Paniqui, Tarlac, Philippines",
-        gpa: "4.0 (A+ in Pranks)",
-        thesis: "The Art of Subverting Expectations: A Practical Guide",
+        gpa: "",
+        thesis: "",
       },
-      {
-        title: "Diploma in Information System Technology",
-        date: "2019 - Present",
-        location: "Philippines Womens' University, Tarlac City, Philippines",
-        gpa: "4.0 (A+ in Pranks)",
-        thesis: "Barangay Web Application Information and Document Management System",
-      },
+      
     ],
     experience: [
       {
@@ -36,7 +43,7 @@ export const info = {
         date: "2023 - Present",
         location: "Central Luzon Doctors' Hospital, Tarlac City, Philippines",
         description:
-          "Crafted interactive web experiences using React and Redux. Often multitasked between coding and avoiding Principal Skinner.",
+          "Crafted interactive web and desktop experiences using NextJS, Prisma, Socket.IO, Zustand, WinForms, ASP.NET Core Web API, C#. Often multitasked between coding and supporting with IT Department.",
       },
     ],
 
@@ -45,23 +52,33 @@ export const info = {
 
   projects: [
     {
-      title: "Bart's Prank Portfolio",
+      title: "Wellcare API ",
       date: "2024",
       description:
-        "An interactive portfolio website showcasing my mischievous coding projects.",
+        "An API System",
       link: "https://github.com/bartsimpsonfake/prank-portfolio",
       tech: ["React", "Node.js", "MongoDB", "Sarcasm.js"],
       img_alt: "Bart's Portfolio",
       img_path: "/simpsons.png",
     },
     {
-      title: "Skateboard Shop",
+      title: "Case Mix Collation System",
+      date: "2024",
+      description:
+        "A New module for Legacy System that alters a data to provide for a census report, altering the data by correcting the proper Cases of Patients to their appropriate Case of their Doctor Specialization.",
+      link: "https://github.com/bartsimpsonfake/skate-shop",
+      tech: ["SQL Server", "WinForms", "C#"],
+      img_alt: "Case Mix",
+      img_path: "skateboard.png",
+    },
+    {
+      title: "Patient Queueing System (CLDH)",
       date: "2023",
       description:
-        "An e-commerce site selling skateboards and prank supplies. No bullies allowed!",
+        "Queueing Management System integrated to BizBox (Healthcare Information System) Facilitating the Queueing of Patients per Departments and Inter-Departments, Generating Reports and maintaining Real-Time Web Socket Connection",
       link: "https://github.com/bartsimpsonfake/skate-shop",
-      tech: ["React", "Node.js", "MongoDB", "Skateboard.js"],
-      img_alt: "Skateboard Shop",
+      tech: ["NextJS", "Prisma", "SQL Server", "WinForms", "C#"],
+      img_alt: "Queueing Collage",
       img_path: "skateboard.png",
     },
     {
